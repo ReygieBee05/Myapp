2f27609f8a95bb4a73dabc3c707dace7a6720acc
Reygie <reygiesales52@gmail.com>
2026-05-27 19:33:27 +0800

    Fix Inertia profile middleware setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,13 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->trustProxies(
-        at: '*',
-        headers: Request::HEADER_X_FORWARDED_FOR
-            | Request::HEADER_X_FORWARDED_HOST
-            | Request::HEADER_X_FORWARDED_PORT
-            | Request::HEADER_X_FORWARDED_PROTO
-    );
+    $middleware->web(append: [
+        \App\Http\Middleware\HandleInertiaRequests::class,
+    ]);
 })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 Route::get('/api/posts', [PostController::class, 'index']);
 Route::post('/api/posts', [PostController::class, 'store']);
 Route::put('/api/posts/{post}', [PostController::class, 'update']);
 Route::delete('/api/posts/{post}', [PostController::class, 'destroy']);
 Route::post('/api/posts/{post}/comments', [PostController::class, 'storeComment']);
 Route::delete('/api/comments/{comment}', [PostController::class, 'destroyComment']);
-Route::delete('/api/comments/{comment}', [PostController::class, 'destroyComment']);
+
 require __DIR__.'/auth.php';
